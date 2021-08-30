85898c1c5b57764f9c57625ca15dd363cd4db1aa
Woody Lam <woody-lam-cwl@gmail.com>
2021-08-30 10:25:42 +0100

    Setup basic express server

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log('Hello world!');
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (req, res) => res.send('Hello world!'));
+
+app.listen(PORT, () => console.log(`Server is running at PORT ${PORT}`));
